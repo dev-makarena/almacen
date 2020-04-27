90ff8390f1a5ee698e5b138b4c0dc1813f78d122
cmallea <cmallea@gmail.com>
2020-04-27 13:11:35 -0400

    Initial commit

---
+++ app/Usuario.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\MyResetPassword;
+
+
+class Usuario extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = 'usuario';
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nombre','password','rut', 'email','numero','id_tipo_user'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function getKeyName(){
+        return "id_user";
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new MyResetPassword($token));
+    }
+
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+
+    public $timestamps = FALSE;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->increments('id_user');
+            $table->string('nombre');
+            $table->string('password');
+            $table->string('rut');
+            $table->string('email');
+            $table->integer('numero');
+            $table->bigInteger('id_tipo_user');
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
