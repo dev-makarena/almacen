42fc03738c11901c834ee4386f5292b29061db04
makarena.212@gmail.com <makarena.212@gmail.com>
2020-04-28 00:05:11 -0400

    Cambio de nombres y eliminacion de recuerda contraseña

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Inició Sesión') }}</div>
+                <div class="card-header">{{ __('Inicio Sesión') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -41,13 +41,7 @@
 
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Recuérdame') }}
-                                    </label>
-                                </div>
+                                
                             </div>
                         </div>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('Registro') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -82,7 +82,7 @@
                         </div>
 
                         <div class="form-group row">
-                        	<label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                        	<label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirme Password') }}</label>
 
                         	<div class="col-md-6">
                         		<input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
